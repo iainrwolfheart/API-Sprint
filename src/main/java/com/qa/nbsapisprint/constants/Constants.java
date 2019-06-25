b2a76f875d2b0a14035985d531226269bb57c93b
mdh111 <mikeyhill@gmail.com>
2019-06-25 10:07:10 +0100

    Constants added

---
+++ src/main/java/com/qa/nbsapisprint/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.qa.nbsapisprint.constants;
+
+public class Constants {
+	public static final String FRIENDS = "/friends";
+	public static final String FRIENDS_WITH_PARAM = "/friends/{id}";
+}
+
